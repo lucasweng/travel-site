c2fd1b910b6b1898e7827b0bd053b774503c2596
Lucas Weng <30640930+lucasweng@users.noreply.github.com>
2017-09-27 16:29:21 +0800

    Completed large hero block and intro content block.

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+
+gulp.task('watch', function() {
+  browserSync.init({
+    notify: false, // turn off the browser-sync notification
+    server: {
+      baseDir: "app"
+    }
+  });
+  gulp.watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+  gulp.watch('./app/assets/styles/**/*.css', ['cssInject']);
+});
+
+//take the content of our compiled css file
+//hand that over to browser-sync so that
+//it can inject those style into the page on the fly
+//['styles'] means the 'styles' task have to be run first
+//before this cssInject task
+gulp.task('cssInject', ['styles'], function() {
+
+  //async function pointing toward the source
+  return gulp.src('./app/temp/styles/style.css')
+    .pipe(browserSync.stream());
+    //make the content piped and available in the browser
+});
---
+++ gulpfile.js
@@ -0,0 +1,2 @@
+require('./gulp/tasks/style');
+require('./gulp/tasks/watch');
